f81db8a30b04d4ae166c3bc44fd0d2a6b1aa0d1f
ktripathi2611 <tripathikushal522@gmail.com>
2024-09-08 01:32:22 +0530

    setup template

--- myproject/views.py
+++ myproject/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 def home(request):
-    return HttpResponse("hello u are at the home page")
+    # return HttpResponse("hello u are at the home page")
+    return render(request, 'index.html')
 def about(request):
     return HttpResponse("hello u are at the about page")
 def contact(request):
