4bb852069a969520123aa6dec7f9773b3f14817c
ktripathi2611 <tripathikushal522@gmail.com>
2024-09-08 20:21:22 +0530

    Restart everything

--- myproject/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-def home(request):
-    # return HttpResponse("hello u are at the home page")
-    return render(request, 'index.html')
-def about(request):
-    return HttpResponse("hello u are at the about page")
-def contact(request):
-    return HttpResponse("hello u are at the contact page")
